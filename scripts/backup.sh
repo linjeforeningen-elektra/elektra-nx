d1d8d07682208a2aa13ea7d40b97b78ee752b95b
Andreas Ipsen <k0rrupt.dev@gmail.com>
2023-01-08 14:49:50 +0100

    updated backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 #load variables
-export $(grep -v '^#' .env | xargs)
+# export $(grep -v '^#' .env | xargs)
+set -a
+source <(cat .env | sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
+set +a
 
 docker exec postgres /bin/sh -c "pg_dump -p $DATABASE_PORT -U $DATABASE_USERNAME -d $DATABASE_DATABASE > /usr/src/backups/backup-$(date +"%Y-%m-%d-%T").sql"
